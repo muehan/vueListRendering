489eca509e51a9afff1ff2c7e1d7293c4c6e81f7
andre <tdc@gmx.ch>
2017-02-05 16:39:34 +0100

    save to change computer...

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,18 @@
 </template>
 
 <script>
-import Hello from './components/Hello'
+    import Screen from './components/Screen'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+    import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+    export default {
+        name: 'app',
+        components: {
+            Screen
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
