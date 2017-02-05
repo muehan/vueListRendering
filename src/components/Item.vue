030c6d27300b37244eba6a4d6b9e92c86fbd1f57
André <tdc@gmx.ch>
2017-02-05 17:24:21 +0100

    add list which works

---
+++ src/components/Item.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="jumbotron">
+        <p>{{item.key}}</p>
+        <p>{{item.text}}</p>
+        <button class="btn btn-primary" @click="remove">Remove</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'item',
+        props: ['item', 'removeItem'],
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            remove() {
+                this.removeItem(this.item);
+            }
+        },
+    }
+</script>
--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -1,38 +1,55 @@
 <template>
   <div class="container">
-      <div class=".col-md-4"></div>
+      <div class="col-md-4">
+          <item v-for="item in items" :item="item" v-bind:removeItem="removeItem" />
+      </div>
   </div>
 </template>
 
 <script>
+    import Item from './Item'
+
     export default {
         name: 'screen',
         data() {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                items: [{
+                    key: "1",
+                    text: "asdfasdf"
+                }, {
+                    key: "2",
+                    text: "lore ipsum"
+                }, {
+                    key: "3",
+                    text: "qwertz"
+                }, {
+                    key: "4",
+                    text: "sdfgdfgsdfg"
+                }, {
+                    key: "5",
+                    text: "dfg gaertgsd"
+                }, {
+                    key: "6",
+                    text: "fhh5 stg egbv erg"
+                }]
             }
+        },
+        methods: {
+            removeItem(itemToRemove) {
+                this.items = this.items.filter((i) => {
+                    return i.key !== itemToRemove.key
+                });
+            },
+        },
+        components: {
+            Item
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h1,
-    h2 {
-        font-weight: normal;
-    }
-    
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    
-    a {
-        color: #42b983;
+    .container {
+        margin-top: 25px;
     }
 </style>
